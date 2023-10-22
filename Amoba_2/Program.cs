b0bc2deaffb83dcf3cce0ab944afc3bc673d7278
Osz Istvan <oszistvan@hotmail.com>
2023-10-22 23:08:47 +0200

    Verzio 5 - Dinamikus oszlopnevek

--- Amoba_2/Program.cs
+++ Amoba_2/Program.cs
@@ -37,11 +37,19 @@ namespace Amoba_2
         public static void PalyaKirajzol()
         {
             Console.Clear();
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < palya.GetLength(0); i++)
             {
-                //if (i == 0) { Console.Write("|"); }
+                int n = 0;
+                for (int j = 0; j < palya.GetLength(1); j++)
+                {
+                    if (i == 0&&n == 0) { Console.Write("  "); }
+                    if (i == 0) { Console.Write("|_" + Convert.ToChar(65 + n) + "_"); }
+                    n++;
+                    if (i == 0 && n == palya.GetLength(0)) { Console.Write("|"); Console.WriteLine(); }
+                }
                 for (int j = 0; j < 3; j++)
                 {
+                    if (j == 0) { Console.Write((i + 1).ToString() +" "); }
                     if (palya[i, j] == "0") { Console.Write("|___"); }
                     else if (palya[i, j] == "1") { Console.Write("|_X_"); }
                     else if (palya[i, j] == "2") { Console.Write("|_O_"); }
@@ -62,52 +70,125 @@ namespace Amoba_2
             }
         }
 
-        public static int KiNyert()
+        public static int NyeroSor( int sor )
         {
-            // TODO!!!
-            int kiNyert = 0;
-            if (palya[0, 0] == "X" && palya[0, 1] == "X" && palya[0, 2] == "X") { kiNyert = 1; }
-            else if (palya[0, 0] == "O" && palya[0, 1] == "O" && palya[0, 2] == "O") { kiNyert = 2; }
-            return kiNyert;
+            bool nyert1 = false;
+            bool nyert2 = false;
+            for (int j = 0; j < palya.GetLength(1)-2; j++)
+            {
+                if (palya[sor, j] == "1" && palya[sor,j+1]=="1" && palya[sor,j+2]=="1") { nyert1 = true; }
+                if (palya[sor, j] == "2" && palya[sor, j + 1] == "2" && palya[sor, j + 2] == "2") { nyert2 = true; }
+            }
+            if (nyert1) { return 1; }
+            else if (nyert2) { return 2; }
+            else { return 0;  }
         }
 
-        public static void LepesRegisztral(string lepes)
+        public static int NyeroOszlop(int oszlop)
         {
-            //  TODO!!!
-            //Console.WriteLine("Léptem");
-            //palya[1, 2] = Convert.ToString(aktualisJatekos);
-            string[] Lepes = lepes.Split(' ');
-            string Hlepes = Lepes[0];
-            int HLepes=0;
-            switch (Hlepes)
+            bool nyert1 = false;
+            bool nyert2 = false;
+            for (int i = 0; i < palya.GetLength(0) - 2; i++)
             {
-                case "a": HLepes = 0; break;
-                case "b": HLepes = 1; break;
-                case "c": HLepes = 2; break;
-                default: Console.WriteLine("Nem megfelelő adatbevitel!/nKérem próbálja újra!"); break;
+                if (palya[i, oszlop] == "1" && palya[i+1, oszlop] == "1" && palya[i+2, oszlop] == "1") { nyert1 = true; }
+                if (palya[i, oszlop] == "2" && palya[i+1, oszlop] == "2" && palya[i+2, oszlop] == "2") { nyert2 = true; }
             }
-            int VLepes = Convert.ToInt32(Lepes[1]);
-            palya[HLepes, VLepes] = Convert.ToString(aktualisJatekos); 
+            if (nyert1) { return 1; }
+            else if (nyert2) { return 2; }
+            else { return 0; }
         }
+
+        public static int NyeroAtlo( int atlo )
+        {
+            return 0;
+        }
+
+        public static int KiNyert()
+        {
+            bool nyert1 = false;
+            bool nyert2 = false;
+            int nyertes, i, j;
+
+            for ( i = 0; i < palya.GetLength(0); i++)
+            {
+                if ((nyertes = NyeroSor(i)) > 0) { return nyertes; }
+            }
+            for (j = 0; j < palya.GetLength(1); j++)
+            {
+                if ((nyertes = NyeroOszlop(j)) > 0) { return nyertes; }
+            }
+            for (i = 0; i < palya.GetLength(1); i++)
+            {
+                if ((nyertes = NyeroAtlo(i)) > 0) { return nyertes; }
+            }
+            return 0;
+        }
+
+        public static (bool, int, int) ErvenyesLepes(string lepes)
+        {
+            char[] Lepes = new char[2];
+            Lepes = lepes.ToCharArray();
+            char Hkar = ' ';
+            int Hint = 0;
+            int Vint = 0;
+            bool ervenyes = true;
+
+            Hkar = Lepes[0];
+            Vint = Convert.ToInt32(Lepes[1].ToString())-1;
+            switch (Hkar)
+            {
+                case 'a': Hint = 0; break;
+                case 'b': Hint = 1; break;
+                case 'c': Hint = 2; break;
+                default: Hint = 3; break;
+            }
+            if ((Hint > 2 || Hint < 0) || (Vint > 2 || Vint < 0))
+            {        
+                ervenyes = false;
+            }
+
+            if (Convert.ToInt32(palya[Vint, Hint]) > 0)
+                { ervenyes = false;  }
+
+            return (ervenyes, Vint, Hint );
+        }
+
+        public static void JatekosValtas()
+        {
+            if (aktualisJatekos == 1) 
+                { aktualisJatekos = 2; }
+            else 
+                { aktualisJatekos = 1; }
+        }
+
+        public static string AktualisJatekosNeve()
+        {
+            if (aktualisJatekos == 1)
+                { return Jatekos1;  }
+            else
+                { return Jatekos2; }
+        }
+
         public static void KovetkezoLepesBeker()
         {
-            if (aktualisJatekos == 1) { 
-                Console.WriteLine("Kedves "+Jatekos1+" kérem lépjen! A mező koordinátáit szóközzel elválasztva adja meg!"); 
-                string lepes=Console.ReadLine();
-                LepesRegisztral(lepes);
-                aktualisJatekos = 2;
-            }
-            else if (aktualisJatekos == 2)
+            bool ervenyes; int x; int y;
+            do
             {
-                Console.WriteLine("Kedves " + Jatekos2 + " kérem lépjen! A mező koordinátáit szóközzel elválasztva adja meg!");
-                string lepes = Console.ReadLine();
-                LepesRegisztral(lepes);
-                aktualisJatekos = 1;
-            }
+                Console.WriteLine("Kedves " + AktualisJatekosNeve() + " kérem lépjen!");
+                string lepes = Console.ReadLine().ToLower();
+                (ervenyes, x, y) = ErvenyesLepes(lepes);
+                if (!ervenyes)
+                    { Console.WriteLine("Ervenytelen lepes, kérlek újra add meg a lépésed!"); }
+            } while (!ervenyes);
+            palya[x, y] = Convert.ToString(aktualisJatekos);
+
+            JatekosValtas();
+
         }
         public static void GyoztesDicser()
         {
-            Console.WriteLine("Nagyon szuper vagy!");
+            int kiNyert = KiNyert();
+            Console.WriteLine("Nagyon szuper vagy {0}!", kiNyert == 1 ? Jatekos1 : Jatekos2);
         }
 
         static void Main(string[] args)
